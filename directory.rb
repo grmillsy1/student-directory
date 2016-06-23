19e4ffdc4a6c2cc9b3ce165e09dbcd259128f99b
Georgia Mills <georgee_123@hotmail.com>
2016-06-23 20:59:55 +0100

    Exercise 8.2displaying a name defined by a letter

--- directory.rb
+++ directory.rb
@@ -1,9 +1,11 @@
 
+
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   students = []
-  name = gets.chomp
+  name = gets.chomp.capital
   while !name.empty? do
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} student"
@@ -17,10 +19,12 @@ puts "The students of Villains Academy"
 puts "________"
 end
 
-def print(students)
-students.each.with_index(1) do |student, index|
-  puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
-end
+def print_names(students)
+  students.each.with_index(1) do |student, index|
+     if student[:name][0] == "G"
+     puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
 end
 
 def print_footer(students)
@@ -29,5 +33,5 @@ end
 
 students = input_students
 print_header
-print(students)
+print_names(students)
 print_footer(students)
