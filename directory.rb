5107fcf9a0fd20090c665dd1698bea100c6bbdc4
Georgia Mills <georgee_123@hotmail.com>
2016-06-24 14:53:43 +0100

    edited code for adding hobbies..

--- directory.rb
+++ directory.rb
@@ -2,13 +2,13 @@
 
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the name of your first student"
+
   students = []
   name = gets.chomp
   while !name.empty? do
     puts "great, thanks!"
-    puts "and what date was #{name} born?"
+    puts "and what date was #{name} born (DD/MM/YY)?"
     date = gets.chomp
     puts "Thanks! Do you know if #{name} had any hobbies?"
     hobbies = gets.chomp
@@ -17,6 +17,8 @@ def input_students
 
     students << {name: name, cohort: :november, dob: date, hobby: hobbies, heights: height}
     puts "Now we have #{students.count} student"
+    puts "Do you want to add any more students?"
+    puts "if not, please hit return twice"
     name = gets.chomp
   end
   students
