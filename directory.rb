a4e4e508c999f4dc95d89852aa33e8d6afa81ce1
Georgia Mills <georgee_123@hotmail.com>
2016-06-24 14:44:57 +0100

    added height, hobbies and dob options

--- directory.rb
+++ directory.rb
@@ -7,7 +7,15 @@ def input_students
   students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    puts "great, thanks!"
+    puts "and what date was #{name} born?"
+    date = gets.chomp
+    puts "Thanks! Do you know if #{name} had any hobbies?"
+    hobbies = gets.chomp
+    puts "what about #{name}\'s height?"
+    height = gets.chomp
+
+    students << {name: name, cohort: :november, dob: date, hobby: hobbies, heights: height}
     puts "Now we have #{students.count} student"
     name = gets.chomp
   end
@@ -20,7 +28,7 @@ puts "________"
 end
 
 def print_names(students)
-index = 0 
+index = 0
 while index < students.length
   student = students[index]
      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
